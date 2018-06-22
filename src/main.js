eb8433bfeae339b05bc4cfee1f4e7c5502d483c1
[zcx] <[1787697839@qq.com]>
2018-06-22 16:40:27 +0800

    侧边栏组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 程序入口
 import Vue from 'vue';
-import App from './App';
+// 根组件
+import App from './App.vue';
+// 路由包，必须包含index.js
 import router from './router';
+
+// 导入bootstrap样式,默认会去node_中找
+import 'bootstrap/dist/css/bootstrap.css';
+// 导入自定义样式
+import '@/assets/css/index.css';
 
 Vue.config.productionTip = false;
 
