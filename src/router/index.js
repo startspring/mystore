657ca1d0166d00551dbd308980632f7c825f3192
[zcx] <[1787697839@qq.com]>
2018-06-22 21:37:17 +0800

    英雄列表

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+//加载路由对应的组件
+import HeroesList from '@/views/heroes/list';
+
 // 注册vue-router插件
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {name: 'home', path:'/', redirect:  '/heroes'},默认路径指向
+    {name: 'home', path:'/', redirect:  {name: '/heroes'} },
+    {name: 'heroes', path:'/heroes', component:  HeroesList}
   ]
 });
