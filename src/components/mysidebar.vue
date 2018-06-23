d3878ecf8133860caaf1f4c15349a1a7def864ef
[zcx] <[1787697839@qq.com]>
2018-06-23 17:14:51 +0800

    英雄列表

--- src/components/mysidebar.vue
+++ src/components/mysidebar.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">英雄管理<span class="sr-only">(current)</span></a></li>
-            <li><a href="#">武器管理</a></li>
-            <li><a href="#">装备管理</a></li>
+            <router-link tag="li" to="/heroes" active-class="active">
+              <a href="#">英雄管理</a>
+            </router-link>
+            <router-link tag="li" to="/weapons" active-class="active">
+              <a href="#">武器管理</a>
+            </router-link>
+            <router-link tag="li" to="/equips" active-class="active">
+              <a href="#">装备管理</a>
+            </router-link>
           </ul>
     </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-//加载路由对应的组件
+// 加载路由对应的组件
 import HeroesList from '@/views/heroes/list';
+import EquipsList from '@/views/equips/list';
+import WeaponsList from '@/views/weapons/list';
 
 // 注册vue-router插件
 Vue.use(Router);
@@ -10,7 +12,9 @@ Vue.use(Router);
 export default new Router({
   routes: [
     // {name: 'home', path:'/', redirect:  '/heroes'},默认路径指向
-    {name: 'home', path:'/', redirect:  {name: '/heroes'} },
-    {name: 'heroes', path:'/heroes', component:  HeroesList}
+    {name: 'home', path: '/', redirect: {name: 'heroes'}},
+    {name: 'heroes', path: '/heroes', component: HeroesList},
+    {name: 'equips', path: '/equips', component: EquipsList},
+    {name: 'weapons', path: '/weapons', component: WeaponsList}
   ]
 });
